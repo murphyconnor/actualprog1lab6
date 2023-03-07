4791f9a791a66563bc7a14f205ff72a2682af819
murphyconnor <connorm5701@comcast.net>
2023-03-07 12:11:51 -0500

    Second commit

--- main.py
+++ main.py
@@ -20,11 +20,23 @@ def decoder(input_string):
         decoded_string += char
     return decoded_string
 
+def printmenu():
+    print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+
 def main():
-    new_string = encoder(input("Enter an eight-digit string of numbers:"))
-    print(f"Encoded password: {new_string} ")
-    decoded_string = decoder(new_string)
-    print(f"Decoded password: {decoded_string} ")
+    while True:
+        printmenu()
+        print()
+        user_input = int(input("Please enter an option: "))
+        if user_input == 1:
+            input_string = input("Please enter your password to encode: ")
+            new_string = encoder(input_string)
+            print("Your password has been encoded and stored!")
+        elif user_input == 2:
+            decoded_string = decoder(new_string)
+            print(f"The encoded password is {new_string}, and the original password is {decoded_string}.")
+        elif user_input == 3:
+            break
 
 # comment
 if __name__ == "__main__":
