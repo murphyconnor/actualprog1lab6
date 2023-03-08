e62bb4ecf62b01347c734d6bad3c3c9ff89021b0
frchalfoun <frchalfoun@gmail.com>
2023-03-08 17:04:30 -0500

    Finalized Decoder - Francesca

--- main.py
+++ main.py
@@ -10,19 +10,29 @@ def encoder(input_string):
         new_string += char
     return new_string
 
-def decoder(input_string):
-    decoded_string = ""
-    for char in input_string:
+
+def decoder(new_string):
+    # converts encoder result into an iterable list to be used in the for loop
+    encoded_string_list = list(new_string)
+    decoded_string = ''
+    # loops through each character(number) of the encoded password and performs appropriate operation to decode
+    for char in encoded_string_list:
         char = int(char)
-        char -= 3
-        if char <= 0:
-            char = char  + 10
-        char = str(char)
-        decoded_string += char
+        if 0 <= char <= 2:
+            char += 7
+            char = str(char)
+            decoded_string += char
+        elif 3 <= char <= 9:
+            char -= 3
+            char = str(char)
+            decoded_string += char
+    # returns the decoded password
     return decoded_string
+
 
 def printmenu():
     print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+
 
 def main():
     while True:
@@ -39,7 +49,7 @@ def main():
         elif user_input == 3:
             break
 
+
 # comment
 if __name__ == "__main__":
     main()
-
